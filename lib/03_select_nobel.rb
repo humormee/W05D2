f457df07793803dde2a6be06e3cc7274c50d75d7
Justin Ernst <jernst766@gmail.com>
2021-05-18 14:05:13 -0500

    completed through 04_select second to last method

--- lib/03_select_nobel.rb
+++ lib/03_select_nobel.rb
@@ -61,6 +61,13 @@ end
 def millennial_peace_prizes
   # Give the name of the 'Peace' winners since the year 2000, including 2000.
   execute(<<-SQL)
+  SELECT
+    winner
+  FROM
+    nobels
+  WHERE
+    yr >= 2000 AND
+    subject = 'Peace'
   SQL
 end
 
@@ -68,6 +75,13 @@ def eighties_literature
   # Show all details (yr, subject, winner) of the Literature prize winners
   # for 1980 to 1989 inclusive.
   execute(<<-SQL)
+  SELECT
+    *
+  FROM
+    nobels
+  WHERE
+    subject = 'Literature' AND
+    yr BETWEEN 1980 AND 1989
   SQL
 end
 
@@ -75,11 +89,23 @@ def presidential_prizes
   # Show all details of the presidential winners: ('Theodore Roosevelt',
   # 'Woodrow Wilson', 'Jimmy Carter')
   execute(<<-SQL)
+  SELECT
+    *
+  FROM
+    nobels
+  WHERE
+    winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter')
   SQL
 end
 
 def nobel_johns
   # Show the winners with first name John
   execute(<<-SQL)
+  SELECT
+    winner
+  FROM
+    nobels
+  WHERE
+    winner LIKE 'John%'
   SQL
 end
